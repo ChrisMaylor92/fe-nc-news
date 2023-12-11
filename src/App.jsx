4b652bd3f25b8cb6225a529906d14e79d7f6000d
Chris Maylor <chrismaylor1@hotmail.com>
2023-12-11 14:52:08 +0000

    adds basic home page with rendered articles list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import { Header } from '../components/Header'
+import { Home } from '../components/Home'
 
 function App() {
 
 
   return (
     <>
-      
+      <Header/>
+      <Home/>
     </>
   )
 }
