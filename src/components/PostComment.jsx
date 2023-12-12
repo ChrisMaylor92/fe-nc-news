e446bdeb1f155249395356e6beecd07b2437567d
Chris Maylor <chrismaylor1@hotmail.com>
2023-12-12 19:01:17 +0000

    minor changes

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -1,3 +1,12 @@
 export const PostComment = () => {
-    return <h1>Post Comment</h1>
+    return <div>
+            <h2>Article Title</h2>
+            <h2>Post Comment</h2>
+
+        </div>
 }
+
+
+
+
+///articles/:article_id/comments/post
