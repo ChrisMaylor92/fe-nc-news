92c78ad2a3e925fe8c48846a3327b09d428ac113
Chris Maylor <chrismaylor1@hotmail.com>
2023-12-13 11:22:47 +0000

    refactors article into component makes better use of Optimistic rendering

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,15 +1,14 @@
-import { useParams } from "react-router-dom";
-import { useState } from "react";
-import { useEffect } from "react";
-import { getArticleById, patchArticle } from "../API";
-import { Link } from "react-router-dom";
+import { useParams} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getArticleById} from "../API";
+import { ArticleCard } from "./ArticleCard";
+
 
 export const Article = () => {
     const { article_id } = useParams();
     const [article, setArticle] = useState({})
     const [loading, setLoading] = useState(true);
-    const [hasVoted, setHasVoted] = useState(false)
-    const [voteCount, setVoteCount] = useState(article.votes)
+    
 
     useEffect(() => {
         getArticleById(article_id)
@@ -25,44 +24,5 @@ export const Article = () => {
     if (loading) {
         return <div>Loading!</div>;
     }
-
-    const handleVote = (article_id) => {
-      if (!hasVoted) {
-        setVoteCount((currCount) => currCount + 1)
-        patchArticle(article_id, true)
-        .catch((err) => {
-          setVoteCount((currCount) => currCount - 1)
-          setErr('Something went wrong, please try again.')
-        })
-        setHasVoted(true)
-      }
-      if (hasVoted) {
-        setVoteCount((currCount) => currCount - 1)
-        patchArticle(article_id, false)
-        .catch((err) => {
-          setVoteCount((currCount) => currCount + 1)
-          setErr('Something went wrong, please try again.')
-        })
-        setHasVoted(false)
-      }
-      
-    }
- 
-
-    return <div>
-        <h2>{article.title}</h2>
-        <div className="article-info">
-            <Link to={`/articles/authors/${article.author}`}>Author: {article.author}</Link>
-            <Link to={`/articles/topics/${article.topic}`}>Topic: {article.topic}</Link>
-            <p>Votes: {voteCount}</p>
-            <button onClick={()=> handleVote(article.article_id)}>{hasVoted? Remove : Add} Vote</button>
-            {err ? <p>{err}</p> : null}
-        </div>
-        
-        <img className="article-img" src={article.article_img_url}></img>
-        <p>{article.body}</p>
-        <div>
-            <Link to={`/articles/${article.article_id}/comments`}>Comments {article.comment_count}</Link>
-        </div>
-    </div>
+    return <ArticleCard article={article}/>
 }
---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,47 @@
+import { patchArticle } from "../API"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export const ArticleCard = ({article}) => {
+    console.log(article)
+    const [hasVoted, setHasVoted] = useState(false)
+    const [voteCount, setVoteCount] = useState(article.votes)
+    const [err, setErr] = useState(null)
+
+    const handleVote = () => {
+        if (!hasVoted) {
+          setVoteCount((currCount) => currCount + 1)
+          patchArticle(article.article_id, true)
+          .catch((err) => {
+            setVoteCount((currCount) => currCount - 1)
+            setErr('Something went wrong, please try again.')
+          })
+          setHasVoted(true)
+        }
+        if (hasVoted) {
+          setVoteCount((currCount) => currCount - 1)
+          patchArticle(article.article_id, false)
+          .catch((err) => {
+            setVoteCount((currCount) => currCount + 1)
+            setErr('Something went wrong, please try again.')
+          })
+          setHasVoted(false)
+        }
+    }
+    console.log(article.title)
+    return (
+        <div>
+        <h2>{article.title}</h2>
+        <div className="article-info">
+            <Link to={`/articles/authors/${article.author}`}>Author: {article.author}</Link>
+            <Link to={`/articles/topics/${article.topic}`}>Topic: {article.topic}</Link>
+            <p>Votes: {voteCount}</p>
+            <button onClick={handleVote}>{hasVoted? "Remove" : "Add"} Vote</button>
+            {err ? <p>{err}</p> : null}
+        </div>
+        
+        <img className="article-img" src={article.article_img_url}></img>
+        <p>{article.body}</p>
+    </div>
+    )
+}
