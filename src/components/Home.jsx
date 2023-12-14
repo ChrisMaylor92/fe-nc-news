7251780eac25e7f8d1a43f57f3eb297e279655dc
Chris Maylor <chrismaylor1@hotmail.com>
2023-12-14 10:46:49 +0000

    refactors home page to make use of ternary operators rather than if else statements, making it much more concise, ready to use the same code for my articles by topic task

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,55 +36,7 @@ export const Home = () => {
     const previousPage = () => {
         setPageNumber( pageNumber-1 )
     }
-// need to refactor everything below  
-    if (allArticles.length <= 5) {
-        return <div className="home">
-        <h3>Articles</h3>
-        <h3>Sort By Topics/Authors</h3>
-        <Link to={`/articles/post`}>Post New Article</Link>
-        <ul>
-            {articles.map((article) => {
-                return <li key={article.article_id} className="article-list-item">
-                    <div className="article-li-title-info">
-                        <Link  to={`/articles/${article.article_id}`}>{article.title}</Link>
-                        <div className="article-list-item-info">
-                            <Link to={`/articles/topics/${article.topic}`}>Topic: {article.topic}</Link>
-                            <Link to={`/articles/${article.article_id}/comments`}>Comments {article.comment_count}</Link>
-                            <Link to={`/articles/authors/${article.author}`}>Author: {article.author}</Link>
-                        </div>
-                    </div>
-                    <img className="article-list-img"src={article.article_img_url}></img>
-                </li>
-            })}
-        </ul>
-    </div>
-    }
-    if(pageNumber === 1) {
-        return <div className="home">
-        <h3>Articles</h3>
-        <h3>Sort By Topics/Authors</h3>
-        <Link to={`/articles/post`}>Post New Article</Link>
-        <ul>
-            {articles.map((article) => {
-                return <li key={article.article_id} className="article-list-item">
-                    <div className="article-li-title-info">
-                        <Link  to={`/articles/${article.article_id}`}>{article.title}</Link>
-                        <div className="article-list-item-info">
-                            <Link to={`/articles/topics/${article.topic}`}>Topic: {article.topic}</Link>
-                            <Link to={`/articles/${article.article_id}/comments`}>Comments {article.comment_count}</Link>
-                            <Link to={`/articles/authors/${article.author}`}>Author: {article.author}</Link>
-                        </div>
-                    </div>
-                    <img className="article-list-img"src={article.article_img_url}></img>
-                </li>
-            })}
-        </ul>
-        
-        <div className="bottom">
-        <button onClick={nextPage}>Next Page</button>
-        </div>
-    </div>
-    }
+
 
     const remainder = allArticles.length % 5
     const listNoRemainder = allArticles.length - remainder
@@ -92,81 +44,40 @@ export const Home = () => {
 
 
 
-    if (remainder === 0 && pageNumber === fullPages) {
-                return <div className="home">
-                <h2>Articles</h2>
-                <h3>Sort By Topics/Authors</h3>
-                <Link to={`/articles/post`}>Post New Article</Link>
-                <ul>
-                    {articles.map((article) => {
-                        return <li key={article.article_id} className="article-list-item">
-                            <div className="article-li-title-info">
-                                <Link  to={`/articles/${article.article_id}`}>{article.title}</Link>
-                                <div className="article-list-item-info">
-                                    <Link to={`/articles/topics/${article.topic}`}>Topic: {article.topic}</Link>
-                                    <Link to={`/articles/${article.article_id}/comments`}>Comments {article.comment_count}</Link>
-                                    <Link to={`/articles/authors/${article.author}`}>Author: {article.author}</Link>
-                                </div>
-                            </div>
-                            <img className="article-list-img"src={article.article_img_url}></img>
-                        </li>
-                    })}
-                </ul>
-                <div className="bottom">
-                <button onClick={previousPage}>Previous Page</button>
-                </div>
-            </div>
-        }
-        if (remainder > 0 && pageNumber === fullPages + 1) {
-            return <div className="home">
-            <h2>Articles</h2>
-            <h3>Sort By Topics/Authors</h3>
-            <Link to={`/articles/post`}>Post New Article</Link>
-            <ul>
-                {articles.map((article) => {
-                    return <li key={article.article_id} className="article-list-item">
-                        <div className="article-li-title-info">
-                            <Link  to={`/articles/${article.article_id}`}>{article.title}</Link>
-                            <div className="article-list-item-info">
-                                <Link to={`/articles/topics/${article.topic}`}>Topic: {article.topic}</Link>
-                                <Link to={`/articles/${article.article_id}/comments`}>Comments {article.comment_count}</Link>
-                                <Link to={`/articles/authors/${article.author}`}>Author: {article.author}</Link>
-                            </div>
-                        </div>
-                        <img className="article-list-img"src={article.article_img_url}></img>
-                    </li>
-                })}
-            </ul>
-            <div className="bottom">
-            <button onClick={previousPage}>Previous Page</button>
-            </div>
-        </div>
-    }
-    
     return <div className="home">
-        <h2>Articles</h2>
-        <h3>Sort By Topics/Authors</h3>
-        <Link to={`/articles/post`}>Post New Article</Link>
-        <ul>
-            {articles.map((article) => {
-                return <li key={article.article_id} className="article-list-item">
-                    <div className="article-li-title-info">
-                        <Link  to={`/articles/${article.article_id}`}>{article.title}</Link>
-                        <div className="article-list-item-info">
-                            <Link to={`/articles/topics/${article.topic}`}>Topic: {article.topic}</Link>
-                            <Link to={`/articles/${article.article_id}/comments`}>Comments {article.comment_count}</Link>
-                            <Link to={`/articles/authors/${article.author}`}>Author: {article.author}</Link>
-                        </div>
+    <h2>Articles</h2>
+    <h3>Sort By Topics/Authors</h3>
+    <Link to={`/articles/post`}>Post New Article</Link>
+    <ul>
+        {articles.map((article) => {
+            return <li key={article.article_id} className="article-list-item">
+                <div className="article-li-title-info">
+                    <Link  to={`/articles/${article.article_id}`}>{article.title}</Link>
+                    <div className="article-list-item-info">
+                        <Link to={`/articles/topics/${article.topic}`}>Topic: {article.topic}</Link>
+                        <Link to={`/articles/${article.article_id}/comments`}>Comments {article.comment_count}</Link>
+                        <Link to={`/articles/authors/${article.author}`}>Author: {article.author}</Link>
                     </div>
-                    <img className="article-list-img"src={article.article_img_url}></img>
-                </li>
-            })}
-        </ul>
-        <div className="bottom">
-        <button onClick={previousPage}>Previous Page</button>
-        <button onClick={nextPage}>Next Page</button>
-        </div>
+                </div>
+                <img className="article-list-img"src={article.article_img_url}></img>
+            </li>
+        })}
+    </ul>
+    <div className="bottom">
+        {allArticles.length <= 5 || pageNumber === 1 ? null : <button onClick={previousPage}>Previous Page</button>}
+        {allArticles.length <= 5 || remainder === 0 && pageNumber === fullPages || remainder > 0 && pageNumber === fullPages + 1 ? null : <button onClick={nextPage}>Next Page</button>}
     </div>
+    </div>
+
 }
+
+
+
+
+
+
+
+
+
 
 
